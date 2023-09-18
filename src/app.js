391f5fd35fab19c26774fa9fcc3a1f253d3a4588
Reedtztoja <patryk.patryg@gmail.com>
2023-09-18 12:56:29 +0200

    dziala

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
+
+const app = express();
+
+app.set('views', './src/views');
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+   // res.send("Strona dziala");
+   res.render('index', {});
+});
+
+app.get('/omnie', (req, res) => {
+    res.render('omnie', {});
+});
+
+app.get('/kontakt', (req, res) => {
+    res.render('kontakt', {});
+});
+
+
+app.listen(5000, () => {
+    console.log("Serwer dziala");
+});
